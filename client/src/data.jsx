9a5aa1473eeefb45d309352e3b21e2863c366848
Devin O'Brien <61803565+devinobrien-css@users.noreply.github.com>
2022-12-02 14:48:38 -0500

    updates

---
+++ client/src/data.jsx
@@ -0,0 +1,65 @@
+export 
+const tempCategoryData = [
+  {
+    name:"Front-End",
+    parentCategories:[
+
+    ],
+    childCategories:[
+      {
+        name:"Frameworks"
+      },
+      {
+        name:"Bundlers"
+      }
+    ],
+    childSkills:[
+    ]
+  },
+  {
+    name:"DBMS",
+    parentCategories:[
+
+    ],
+    childCategories:[
+      {
+        name:"Relational Databases"
+      },
+      {
+        name:"Document Databases"
+      },
+      {
+        name:"Key-Value Stores"
+      }
+    ],
+    childSkills:[
+      
+    ]
+  },
+  {
+    name:"Back-End",
+    parentCategories:[
+
+    ],
+    childCategories:[
+
+    ],
+    childSkills:[
+
+    ]
+  },
+  {
+    name:"Javascript",
+    parentCategories:[
+
+    ],
+    childCategories:[
+
+    ],
+    childSkills:[
+        {
+            name:"Javascript"
+        }
+    ]
+  }
+]
