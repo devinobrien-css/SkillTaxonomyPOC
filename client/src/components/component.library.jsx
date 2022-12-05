524a388e7e147cc6327234ae46e057aa59da49e0
Devin O'Brien <61803565+devinobrien-css@users.noreply.github.com>
2022-12-05 11:18:17 -0500

    skills page initialized for query integration

--- client/src/components/component.library.jsx
+++ client/src/components/component.library.jsx
@@ -13,25 +13,90 @@ export const TitleMd = ({children,className,...rest}) => {
     )
 }
 
+export const TitleSm = ({children,className,...rest}) => {
+    return (
+        <p className={`text-xl font-light ${className}`} {...rest}>{children}</p>
+    )
+}
+
 export const SubTitle = ({children,className,...rest}) => {
     return (
         <p className={`text-xl italic text-font-dark font-light ${className}`} {...rest}>{children}</p>
     )
 }
 
-export const SearchSelect = ({className,options,...rest}) => {
+export const ButtonMd = ({children,className,color,...rest}) => {
+    const colorways = {
+        red:"text-white bg-red-300",
+        black:"text-white bg-black",
+        blue:"text-white bg-blue-400",
+    }
+
+    return (
+        <button
+            className={`p-3 rounded text-bold text-lg my-2 shadow hover:scale-105 transform transition-all ${colorways[color]}`}
+            {...rest}
+        >
+            {children}
+        </button>
+    )
+}
+
+export const SearchSelect = ({className,options,setSelected,...rest}) => {
     const [search,setSearch] = useState("")
+    const [display,setDisplay] = useState("")
 
 
     return (
 
         <div className="relative border-borders border rounded">
-            <input placeholder="Search..." className="w-full p-2 bg-transparent" onFocus={()=>{}}/>
-            <div className="absolute">
-                {options.map(option =>{
-                    return <p>{option}</p>
+            <input 
+                className="z-50 w-full bg-transparent p-2" 
+                placeholder="Search..." 
+                value={search}
+                onFocus={()=>{
+                    setDisplay(true)
+                }}
+                onBlur={(e)=>{
+                    e.preventDefault()
+                    setTimeout(()=>{
+                        setDisplay()
+                    },"180")
+                }}
+                onChange={(e) => {
+                    setSearch(e.target.value)
+                }}
+            />
+            <div className={`z-30 bg-white top-full absolute p-4 transition-all w-full overflow-y-scroll ${display?"h-72":"h-0 p-0"}`}>
+                {options
+                .filter(option => {
+                    return option.includes(search)
+                })
+                .map((option,index) =>{
+                    return (
+                        <p 
+                            value={option}
+                            key={`option-${index}`}
+                            className="cursor-pointer font-light text-xl border-b p-2"
+                            onClick={() => {
+                                setSearch(option)
+                                setSelected(option)
+                            }}
+                        >
+                            {option}
+                        </p>
+                    )
                 })}
             </div>
         </div>
+    )
+}
+
+
+export const Modal = ({children,className,...rest}) => {
+
+
+    return (
+        <></>
     )
 }
--- client/src/components/exploreSkill.component.jsx
+++ client/src/components/exploreSkill.component.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { SearchSelect, SubTitle, TitleMd } from "./component.library"
+import { ButtonMd, SearchSelect, SubTitle, TitleMd, TitleSm } from "./component.library"
 import { Addbutton } from "./custom.library"
 import { Icon } from '@iconify/react';
 import { tempCategoryData } from "../data";
@@ -7,19 +7,57 @@ import { tempCategoryData } from "../data";
 
 
 
-const AddParent = () => {
+const AddCategoryModal = ({skill}) => {
+    const [selectedCategory,setSelected] = useState()
+
+    console.log(selectedCategory)
     return (
         <div className="p-4">
             <TitleMd>Add a Parent to this Skill</TitleMd>
             <SubTitle>Search and select a category to add this skill to</SubTitle>
-            <SearchSelect options={tempCategoryData.map(category => {return category.name})}/>
+
+            <br/>
+            <br/>
+            <TitleSm>Select a Category</TitleSm>
+            <SearchSelect 
+                options={tempCategoryData.map(category => {return category.name})}
+                setSelected={setSelected}
+            />
+            <br/>
+            <br/>
+
+            {selectedCategory?(
+                <>
+                    <TitleSm>Confirm Selection</TitleSm>
+                    <SubTitle>Add skill <span className="font-bold">{skill.name}</span> to category <span className="font-bold">{selectedCategory}</span>?</SubTitle>
+                    <div className="[&>*]:mx-4">
+                        <ButtonMd
+                            color="blue"
+                        >
+                            Confirm
+                        </ButtonMd>
+                        <ButtonMd
+                            color="red"
+                            onClick={()=>{
+                                setSelected()
+                            }}
+                        >
+                            Cancel
+                        </ButtonMd>
+                    </div>
+                </>
+            ):(
+                <>
+                </>
+            )}  
         </div>
     )
 }
 
 const modalStates = {
-    "add-parent":AddParent
+    "add-parent":AddCategoryModal
 }
+
 
 export const ExploreSkill = ({skill}) => {
     const [modal,setModal] = useState()
@@ -31,7 +69,7 @@ export const ExploreSkill = ({skill}) => {
                 <div className="z-50 absolute h-full w-full bg-gray-400 bg-opacity-70 shadow top-0 left-0 flex flex-col items-center">
                     <div className="bg-white w-1/2 h-1/2 relative shadow-xl my-auto rounded">
                         <Icon icon={"mdi:alpha-x-box-outline"} className="absolute top-1 right-1" width={40} onClick={()=>setModal()}/>
-                        <ModalContent />
+                        <ModalContent skill={skill}/>
                     </div>
                 </div>
             ):(
